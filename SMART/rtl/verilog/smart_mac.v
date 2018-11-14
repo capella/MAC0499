87539f636697db5cd74d5270176a67f1166b5be3
Gabriel Capella <gabriel@capella.pro>
2018-11-14 08:17:08 -0200

    Cleaning smart_mac code.

--- SMART/rtl/verilog/smart_mac.v
+++ SMART/rtl/verilog/smart_mac.v
@@ -36,13 +36,11 @@
 //----------------------------------------------------------------------------
 module  smart_mac (
     // OUTPUTs
-    //=========
     output                  in_safe_area,       // On on safe area
     output                  reset,              // High to reset device
     output           [15:0] mem_dout,           // Memory data output
 
     // INPUTs
-    //=========
     input [SIZE_MEM_ADDR:0] mem_addr,           // Memory adress
     input                   mclk,               // Memory clock
     input            [15:0] mem_din,            // Memory data input
@@ -51,18 +49,17 @@ module  smart_mac (
 );
 
 // PARAMETERs
-//=========
-parameter SIZE_MEM_ADDR = 15;          // size of mem_addr
+//============================================================================
+parameter SIZE_MEM_ADDR = 15;                   // size of mem_addr
 
-parameter LOW_SAFE = 200;              // Low address safe code
-parameter HIGH_SAFE = 200;             // High address safe code
+parameter LOW_SAFE      = 200;                  // Low address safe code
+parameter HIGH_SAFE     = 200;                  // High address safe code
 
-parameter [15:0]LOW_CODE = 200;              // Low adress code
-parameter [15:0]HIGH_CODE = 200;             // High address code
+parameter LOW_CODE      = 200;                  // Low adress code
+parameter HIGH_CODE     = 200;                  // High address code
 
-//=============================================================================
 // LOGIC
-//=============================================================================
+//============================================================================
 reg   inside_code;
 reg   to_be_reset;
 
@@ -77,11 +74,9 @@ assign mem_dout = reset ? 16'b0 : mem_din;
 assign in_safe_area = inside_code;
 
 always @ (posedge mclk) begin
-    if (ins_addr == LOW_CODE)
-        inside_code <= 1'b1;
-    else if (~pc_in_code)
-        inside_code <= 1'b0;
+    if (ins_addr == LOW_CODE)   inside_code <= 1'b1;
+    else if (~pc_in_code)       inside_code <= 1'b0;
     to_be_reset <= safe_reset;
 end
 
-endmodule // smart_mac
+endmodule
